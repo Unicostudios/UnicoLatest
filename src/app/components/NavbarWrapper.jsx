a1ab9813c497ab7c684051aa17aec0bff8262a30
Unico Studios <unicostudioss@gmail.com>
2026-05-02 15:09:19 +0530

    Add '/tools' to hideNavbarOn array

--- src/app/components/NavbarWrapper.jsx
+++ src/app/components/NavbarWrapper.jsx
@@ -1,11 +1,8 @@
 "use client";
-
 import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
-
 export default function NavbarWrapper() {
   const pathname = usePathname();
-  const hideNavbarOn = ["/"];
-
+  const hideNavbarOn = ["/", "/tools"];
   return !hideNavbarOn.includes(pathname) ? <Navbar /> : null;
 }
