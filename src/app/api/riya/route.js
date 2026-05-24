ae1cc6d8cfb79badc3a56e43d4a5404b7569fa6e
Unico Studios <unicostudioss@gmail.com>
2026-05-24 23:50:27 +0530

    Update route.js

--- src/app/api/riya/route.js
+++ src/app/api/riya/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   const body = await req.json();
   const res = await fetch(
-    "https://niquo-bathsy-git-main-unico-studios-projects.vercel.app/api/niquo",
+    "https://niquo-bathsy.vercel.app/api/niquo",
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
